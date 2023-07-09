2f8a15db7be9cf52595ae68e4febea2dd294fc79
buhari36 <buharihassan36@example.com>
2023-07-09 10:41:49 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,143 @@
+import "./home.css";
+import Logo from "../assets/Logo.svg";
+import Rectangle from "../assets/Rectangle.png";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const [mail, setMail] = useState("");
+  const Mails = () => {
+    setMail(!mail);
+  };
+  useEffect(() => {
+    if (mail && enteredNameIsValid && enteredEmailIsValid) {
+      formIsValid = true;
+      alert("Mail Sent");
+    }
+  }, [mail]);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const enteredEmailIsValid = enteredEmail.includes("@");
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  if (enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
+  const emailInputBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    if (!enteredEmailIsValid) {
+      return;
+    }
+
+    setEnteredName("");
+    setEnteredEmail("");
+    setEnteredNameTouched(false);
+    setEnteredEmailTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  return (
+    <div className="container container-home">
+      <div className="home">
+        <div className="left">
+          <div className="image">
+            <img src={Logo} alt="IturaRx logo" />
+            <button className="bon">Coming Soon!</button>
+          </div>
+          <div className="write">
+            <h1>Get Early Access!</h1>
+            <p>
+              Be one of the first to use the IturaRX Pharmacy software when we
+              launch
+            </p>
+          </div>
+          <form onSubmit={formSubmissionHandler}>
+            <div className="inputs ">
+              <div className={nameInputClasses}>
+                <input
+                  type="text"
+                  placeholder="Pharmacy name"
+                  required
+                  value={enteredName}
+                  onChange={nameInputChangeHandler}
+                  onBlur={nameInputBlurHandler}
+                />
+                {nameInputIsInvalid && (
+                  <p className="error-text">Name must not be empty.</p>
+                )}
+              </div>
+              <div className={emailInputClasses}>
+                <input
+                  type="text"
+                  placeholder="Pharmacy Email"
+                  required
+                  value={enteredEmail}
+                  onChange={emailInputChangeHandler}
+                  onBlur={emailInputBlurHandler}
+                />
+                {emailInputIsInvalid && (
+                  <p className="error-text">
+                    Please enter a valid email address.
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="btn">
+              <button disabled={!formIsValid} onClick={Mails}>
+                Join the Waitlist
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="right">
+          <img src={Rectangle} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
